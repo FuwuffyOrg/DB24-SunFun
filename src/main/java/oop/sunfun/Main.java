75896eb90e8e6ae0d89eb11a43840e5846153fdc
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-10 11:42:52 +0200

    Add simple page to test

--- src/main/java/oop/sunfun/Main.java
+++ src/main/java/oop/sunfun/Main.java
@@ -2,42 +2,19 @@ package oop.sunfun;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.UIManager;
+import oop.sunfun.ui.LandingPage;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public final class Main {
-    /**
-     * The window's initial width.
-     */
-    private static final int WINDOW_WIDTH = 1200;
-
-    /**
-     * The window's initial height.
-     */
-    private static final int WINDOW_HEIGHT = 800;
-
-    /**
-     * Constructor used to block other classes to construct Main.
-     */
-    private Main() {
-        // Useless method.
-    }
-
-    /**
-     * The entry point of the program.
-     * @param args The program's arguments.
-     */
     public static void main(final String[] args) {
-        FlatDarkLaf.setup();
         try {
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch (final UnsupportedLookAndFeelException e) {
+            UIManager.setLookAndFeel(new FlatDarkLaf()); // Use FlatDarkLaf for dark theme, or FlatLightLaf for light theme
+        } catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
-        final JFrame frame = new JFrame("SunFun");
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        final JFrame page = new LandingPage();
     }
 }
