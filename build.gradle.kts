c3782519c3bc4d50da36c1cb444b204ea723a895
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-10 10:23:39 +0200

    Add basic checkstyles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
 plugins {
     java
     application
+    checkstyle
+    pmd
+    id("com.github.spotbugs") version "6.0.18"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("${rootDir}/config/checkstyle/checkstyle.xml")
+}
+
+pmd {
+    toolVersion = "7.3.0"
+    // ruleSets = listOf("category/java/bestpractices.xml", "category/java/design.xml")
+}
+
+spotbugs {
+    toolVersion = "6.0.18"
 }
 
 group = "oop.sunfun"
@@ -14,6 +31,8 @@ repositories {
 val supportedPlatforms = listOf("linux", "mac", "win")
 
 dependencies {
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.3")
+
     val jUnitVersion = "5.10.3"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
--- src/main/java/oop/sunfun/Main.java
+++ src/main/java/oop/sunfun/Main.java
@@ -1,6 +1,18 @@
 package oop.sunfun;
 
-public class Main {
+public final class Main {
+
+    /**
+     * Constructor used to block other classes to construct Main.
+     */
+    private Main() {
+        // Useless method.
+    }
+
+    /**
+     * The entry point of the program.
+     * @param args The program's arguments.
+     */
     public static void main(final String[] args) {
         System.out.println("Hello world!");
     }
