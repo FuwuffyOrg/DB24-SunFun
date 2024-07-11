54dadbc4bb35ad5d50ed8c89e6040efff9491951
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-11 11:08:07 +0200

    Improved themeing

--- src/main/java/oop/sunfun/Main.java
+++ src/main/java/oop/sunfun/Main.java
@@ -4,6 +4,9 @@ import com.formdev.flatlaf.intellijthemes.FlatNordIJTheme;
 import oop.sunfun.ui.LoginPage;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Insets;
 
 public final class Main {
     private Main() {
@@ -11,11 +14,37 @@ public final class Main {
     }
 
     /**
+     * Sets up everything to do with theming.
+     */
+    private static void setupTheme() {
+        FlatNordIJTheme.setup();
+        // Rounded borders
+        UIManager.put("Button.arc", 15);
+        UIManager.put("Component.arc", 15);
+        UIManager.put("CheckBox.arc", 15);
+        UIManager.put("ProgressBar.arc", 15);
+        UIManager.put("TextComponent.arc", 15);
+        // Arrows on number stuff
+        UIManager.put("Component.arrowType", "triangle");
+        // Focus size
+        UIManager.put("Component.focusWidth", 2);
+        // Show buttons on scrollbars
+        UIManager.put("ScrollBar.showButtons", true);
+        // Track for scrollbars
+        UIManager.put("ScrollBar.trackArc", 999);
+        UIManager.put("ScrollBar.thumbArc", 999);
+        UIManager.put("ScrollBar.trackInsets", new Insets(2, 4, 2, 4));
+        UIManager.put("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+        // Tab separator stuff
+        UIManager.put("TabbedPane.showTabSeparators", true);
+    }
+
+    /**
      * Entrypoint of the program.
      * @param args The default arguments of any application.
      */
     public static void main(final String[] args) {
-        FlatNordIJTheme.setup();
+        Main.setupTheme();
         final JFrame loginPage = new LoginPage();
     }
 }
