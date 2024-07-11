8ac707bcd3656e84e84204d88c9a0a5f837c83e7
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-11 11:12:38 +0200

    Refactor theming overrides to GenericWindow

--- src/main/java/oop/sunfun/ui/layout/GenericPage.java
+++ src/main/java/oop/sunfun/ui/layout/GenericPage.java
@@ -1,12 +1,16 @@
 package oop.sunfun.ui.layout;
 
+import com.formdev.flatlaf.intellijthemes.FlatNordIJTheme;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 
 public abstract class GenericPage extends JFrame {
@@ -19,6 +23,37 @@ public abstract class GenericPage extends JFrame {
      * The main panel of the window, used to display everything.
      */
     private final JPanel contentPanel;
+
+    /**
+     * Sets up everything to do with theming.
+     */
+    public static void setupTheme() {
+        final int cornersRoundness = 15;
+        final int focusSize = 2;
+        final int scrollbarRoundness = 999;
+        final Insets trackInset = new Insets(2, 4, 2, 4);
+        final Insets thumbInsets = new Insets(2, 2, 2, 2);
+        FlatNordIJTheme.setup();
+        // Rounded borders
+        UIManager.put("Button.arc", cornersRoundness);
+        UIManager.put("Component.arc", cornersRoundness);
+        UIManager.put("CheckBox.arc", cornersRoundness);
+        UIManager.put("ProgressBar.arc", cornersRoundness);
+        UIManager.put("TextComponent.arc", cornersRoundness);
+        // Arrows on number stuff
+        UIManager.put("Component.arrowType", "triangle");
+        // Focus size
+        UIManager.put("Component.focusWidth", focusSize);
+        // Show buttons on scrollbars
+        UIManager.put("ScrollBar.showButtons", true);
+        // Track for scrollbars
+        UIManager.put("ScrollBar.trackArc", scrollbarRoundness);
+        UIManager.put("ScrollBar.thumbArc", scrollbarRoundness);
+        UIManager.put("ScrollBar.trackInsets", trackInset);
+        UIManager.put("ScrollBar.thumbInsets", thumbInsets);
+        // Tab separator stuff
+        UIManager.put("TabbedPane.showTabSeparators", true);
+    }
 
     /**
      * Basic constructor for a generic window in the project.
