028967606d53bfc0d918d4857358194bc0887d5a
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-11 16:20:46 +0200

    Finish register page with query

--- src/main/java/oop/sunfun/ui/RegisterPage.java
+++ src/main/java/oop/sunfun/ui/RegisterPage.java
@@ -1,34 +1,49 @@
 package oop.sunfun.ui;
 
+import oop.sunfun.database.connection.DatabaseConnection;
+import oop.sunfun.database.connection.IDatabaseConnection;
 import oop.sunfun.database.enums.ParentType;
 import oop.sunfun.ui.behavior.CloseEvents;
 import oop.sunfun.ui.layout.GenericPage;
 import oop.sunfun.ui.layout.GridBagConstraintBuilder;
 
-import javax.swing.*;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
 import java.awt.Component;
+import java.sql.SQLException;
 import java.util.EnumSet;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class RegisterPage extends GenericPage {
-    final Component txtName;
-    final Component txtSurname;
-    final Component txtPhone;
-    final Component txtEmail;
-    final Component txtPassword;
-    final Component txtPasswordConfirm;
+    final JTextComponent txtCodiceFiscale;
+    final JTextComponent txtName;
+    final JTextComponent txtSurname;
+    final JTextComponent txtPhone;
+    final JTextComponent txtEmail;
+    final JTextComponent txtPassword;
+    final JTextComponent txtPasswordConfirm;
     final ButtonGroup radParentTypeGroup;
 
     public RegisterPage(final String title, final CloseEvents closeEvent) {
         super(title, closeEvent);
         // Add two labels and text boxes for inputting username and password.
+        final Component lblCodiceFiscale = new JLabel("Codice Fiscale: ");
         final Component lblName = new JLabel("Nome: ");
         final Component lblSurname = new JLabel("Cognome: ");
         final Component lblPhone = new JLabel("Numero di telefono: ");
         final Component lblEmail = new JLabel("Email: ");
         final Component lblPassword = new JLabel("Password: ");
         final Component lblPasswordConfirm = new JLabel("Conferma Password: ");
+        this.txtCodiceFiscale = new JTextField();
         this.txtName = new JTextField();
         this.txtSurname = new JTextField();
         this.txtPhone = new JTextField();
@@ -39,87 +54,101 @@ public final class RegisterPage extends GenericPage {
         final AbstractButton btnRegister = new JButton("Register");
         final AbstractButton btnLogin = new JButton("Goto Login");
         // Add all the components.
-        this.addPanelComponent(lblName,
+        this.addPanelComponent(lblCodiceFiscale,
                 new GridBagConstraintBuilder()
                         .setRow(0).setColumn(0)
-                        .setWidth(4)
+                        .setWidth(3)
+                        .setFillAll()
+                        .build()
+        );
+        this.addPanelComponent(txtCodiceFiscale,
+                new GridBagConstraintBuilder()
+                        .setRow(0).setColumn(3)
+                        .setWidth(3)
+                        .setFillAll()
+                        .build()
+        );
+        this.addPanelComponent(lblName,
+                new GridBagConstraintBuilder()
+                        .setRow(1).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtName,
                 new GridBagConstraintBuilder()
-                        .setRow(0).setColumn(4)
-                        .setWidth(4)
+                        .setRow(1).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(lblSurname,
                 new GridBagConstraintBuilder()
-                        .setRow(1).setColumn(0)
-                        .setWidth(4)
+                        .setRow(2).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtSurname,
                 new GridBagConstraintBuilder()
-                        .setRow(1).setColumn(4)
-                        .setWidth(4)
+                        .setRow(2).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(lblPhone,
                 new GridBagConstraintBuilder()
-                        .setRow(2).setColumn(0)
-                        .setWidth(4)
+                        .setRow(3).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtPhone,
                 new GridBagConstraintBuilder()
-                        .setRow(2).setColumn(4)
-                        .setWidth(4)
+                        .setRow(3).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(lblEmail,
                 new GridBagConstraintBuilder()
-                        .setRow(3).setColumn(0)
-                        .setWidth(4)
+                        .setRow(4).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtEmail,
                 new GridBagConstraintBuilder()
-                        .setRow(3).setColumn(4)
-                        .setWidth(4)
+                        .setRow(4).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(lblPassword,
                 new GridBagConstraintBuilder()
-                        .setRow(4).setColumn(0)
-                        .setWidth(4)
+                        .setRow(5).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtPassword,
                 new GridBagConstraintBuilder()
-                        .setRow(4).setColumn(4)
-                        .setWidth(4)
+                        .setRow(5).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(lblPasswordConfirm,
                 new GridBagConstraintBuilder()
-                        .setRow(5).setColumn(0)
-                        .setWidth(4)
+                        .setRow(6).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(txtPasswordConfirm,
                 new GridBagConstraintBuilder()
-                        .setRow(5).setColumn(4)
-                        .setWidth(4)
+                        .setRow(6).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
@@ -127,11 +156,12 @@ public final class RegisterPage extends GenericPage {
         AtomicInteger index = new AtomicInteger(0);
         EnumSet.allOf(ParentType.class).forEach(p -> {
             final AbstractButton radio = new JRadioButton(p.getTextValue());
+            radio.setActionCommand(p.getTextValue());
             radio.setSelected(true);
             this.radParentTypeGroup.add(radio);
             this.addPanelComponent(radio,
                     new GridBagConstraintBuilder()
-                            .setRow(6).setColumn(index.get())
+                            .setRow(7).setColumn(index.get())
                             .setFillAll()
                             .build()
             );
@@ -139,15 +169,15 @@ public final class RegisterPage extends GenericPage {
         });
         this.addPanelComponent(btnRegister,
                 new GridBagConstraintBuilder()
-                        .setRow(7).setColumn(0)
-                        .setWidth(4)
+                        .setRow(8).setColumn(0)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
         this.addPanelComponent(btnLogin,
                 new GridBagConstraintBuilder()
-                        .setRow(7).setColumn(4)
-                        .setWidth(4)
+                        .setRow(8).setColumn(3)
+                        .setWidth(3)
                         .setFillAll()
                         .build()
         );
@@ -159,6 +189,25 @@ public final class RegisterPage extends GenericPage {
         });
         btnRegister.addActionListener(e -> {
             if (RegisterPage.this.isDataValid()) {
+                final String accountQuery = "INSERT INTO `account`(`email`, `password`, `tipologia`) " +
+                        "VALUES ('%s','%s','Parente')";
+
+                final String parenteQuery = "INSERT INTO `parente`(`codice_fiscale`, `fk_account`, " +
+                        "`nome`, `cognome`, `cellulare`, `grado_di_parentela`) VALUES ('%s','%s','%s','%s','%s','%s')";
+
+                final IDatabaseConnection database = new DatabaseConnection("sunfun",
+                        "jdbc:mysql://localhost", "root", "");
+                try {
+                    database.openConnection();
+                    database.setQueryData(String.format(accountQuery, txtEmail.getText(), txtPassword.getText()));
+                    database.setQueryData(String.format(parenteQuery, txtCodiceFiscale.getText(),
+                            txtEmail.getText(), txtName.getText(), txtSurname.getText(), txtPhone.getText(),
+                            this.radParentTypeGroup.getSelection().getActionCommand()));
+                    database.closeConnection();
+                } catch (final SQLException err) {
+                    err.printStackTrace();
+                    this.close();
+                };
                 final JFrame loginPage = new LoginPage("SunFun Register", CloseEvents.EXIT_PROGRAM);
                 loginPage.setVisible(true);
                 RegisterPage.this.dispose();
@@ -169,6 +218,29 @@ public final class RegisterPage extends GenericPage {
     }
 
     private boolean isDataValid() {
-        return false;
+        final int nameLengthLimit = 36;
+        final int passwordLengthLimit = 24;
+        final int emailLengthLimit = 256;
+        final int codiceFiscaleLength = 16;
+        final int phoneNumberLength = 10;
+        if (this.txtCodiceFiscale.getText().length() != codiceFiscaleLength) {
+            return false;
+        }
+        if (this.txtName.getText().length() > nameLengthLimit || this.txtName.getText().length() < 2) {
+            return false;
+        }
+        if (this.txtSurname.getText().length() > nameLengthLimit || this.txtSurname.getText().length() < 2) {
+            return false;
+        }
+        if (this.txtPassword.getText().length() > passwordLengthLimit || this.txtPassword.getText().length() < 2) {
+            return false;
+        }
+        if (!Objects.equals(this.txtPassword.getText(), this.txtPasswordConfirm.getText())) {
+            return false;
+        }
+        if (this.txtEmail.getText().length() > emailLengthLimit || this.txtEmail.getText().length() < 6) {
+            return false;
+        }
+        return this.txtPhone.getText().length() == phoneNumberLength;
     }
 }
