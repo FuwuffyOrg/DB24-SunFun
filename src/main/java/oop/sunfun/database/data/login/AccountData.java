dcb9eaa7f00cdaa6e8c69800cb5f27a005feafb3
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-12 14:05:06 +0200

    Update AccountData creation process

--- src/main/java/oop/sunfun/database/data/login/AccountData.java
+++ src/main/java/oop/sunfun/database/data/login/AccountData.java
@@ -1,2 +1,23 @@
-package oop.sunfun.database.data.login;public class AccountData {
+package oop.sunfun.database.data.login;
+
+import oop.sunfun.database.data.enums.AccountType;
+
+import java.util.Map;
+
+public class AccountData {
+    private final String email;
+    private final AccountType tipologia;
+
+    public AccountData(final Map<String, Object> sqlData) {
+        this.email = (String) sqlData.get("email");
+        this.tipologia = AccountType.getFromString((String) sqlData.get("tipologia"));
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public AccountType getType() {
+        return this.tipologia;
+    }
 }
