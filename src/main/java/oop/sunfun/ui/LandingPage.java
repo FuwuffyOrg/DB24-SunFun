38d9c1a8c84669e722e193ffb1ed4e90b3c0c1b9
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-12 15:14:50 +0200

    Improve page creation in landing page

--- src/main/java/oop/sunfun/ui/LandingPage.java
+++ src/main/java/oop/sunfun/ui/LandingPage.java
@@ -6,10 +6,13 @@ import oop.sunfun.ui.forum.ForumPage;
 import oop.sunfun.ui.layout.GenericPage;
 import oop.sunfun.ui.layout.GridBagConstraintBuilder;
 
-import javax.swing.*;
-import java.util.HashSet;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class LandingPage extends GenericPage {
@@ -19,22 +22,21 @@ public class LandingPage extends GenericPage {
     public LandingPage(final CloseEvents closeEvent, final AccountData account) {
         super(PAGE_NAME, closeEvent);
         // Set to contain all the pages reachable by that user.
-        final Set<GenericPage> reachablePages = new HashSet<>();
+        final Map<String, Supplier<GenericPage>> reachablePages = new HashMap<>();
         // Add the pages that need to be shown
-        // TODO: Make pages load dynamically
         // TODO: Add constraints depending on the account type
-        reachablePages.add(new ForumPage(CloseEvents.EXIT_PROGRAM));
+        reachablePages.put("Forum", () -> new ForumPage(CloseEvents.EXIT_PROGRAM));
         // Calculate the sides of a rectangle for the button layout
         final double side = Math.sqrt(reachablePages.size());
         final int rows = (int) Math.floor(side);
         final int cols = (int) Math.ceil(side);
         // Add the buttons to the page
-        final Iterator<GenericPage> it = reachablePages.iterator();
+        final Iterator<Entry<String, Supplier<GenericPage>>> it = reachablePages.entrySet().iterator();
         IntStream.range(0, rows).forEach(r -> IntStream.range(0, cols).forEach(c -> {
             if (it.hasNext()) {
-                final GenericPage page = it.next();
-                final AbstractButton btnPage = new JButton(page.getTitle());
-                btnPage.addActionListener(e -> this.switchPage(page));
+                final Entry<String, Supplier<GenericPage>> page = it.next();
+                final AbstractButton btnPage = new JButton(page.getKey());
+                btnPage.addActionListener(e -> this.switchPage(page.getValue().get()));
                 this.add(btnPage, new GridBagConstraintBuilder()
                                 .setRow(r).setColumn(c)
                                 .setMarginAll(8)
