6973e573e8d2320c432bc4565505606c347714f6
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-15 13:56:34 +0200

    Add query functionality to activity page

--- src/main/java/oop/sunfun/database/dao/ActivityDAO.java
+++ src/main/java/oop/sunfun/database/dao/ActivityDAO.java
@@ -2,19 +2,24 @@ package oop.sunfun.database.dao;
 
 import oop.sunfun.database.data.activity.ActivityData;
 import oop.sunfun.database.data.activity.ReviewData;
+import oop.sunfun.database.data.food.AllergenData;
 
+import java.sql.SQLException;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class ActivityDAO extends AbstractDAO {
     private static final Logger LOGGER = Logger.getLogger(ActivityDAO.class.getName());
 
-    private static final String GET_ALL_ACTIVITIES = "";
+    private static final String GET_ALL_ACTIVITIES = "SELECT * FROM `attivita`";
 
-    private static final String CREATE_ACTIVITY = "";
+    private static final String CREATE_ACTIVITY = "INSERT INTO `attivita`(`nome`, `descrizione`) VALUES (?,?)";
 
-    private static final String DELETE_ACTIVITY = "";
+    private static final String DELETE_ACTIVITY = "DELETE FROM `attivita` WHERE `nome`=?";
 
     private static final String GET_ACTIVITY_REVIEWS = "";
 
@@ -22,16 +27,37 @@ public final class ActivityDAO extends AbstractDAO {
 
     public static Set<ActivityData> getAllActivities() {
         final Set<ActivityData> activities = new HashSet<>();
-        // TODO: Complete method and query
+        try {
+            DB_CONNECTION.openConnection();
+            final List<Map<String, Object>> queryData = DB_CONNECTION.getQueryData(GET_ALL_ACTIVITIES);
+            for (final Map<String, Object> activity : queryData) {
+                activities.add(new ActivityData((String) activity.get("nome"), (String) activity.get("descrizione")));
+            }
+        } catch (final SQLException err) {
+            bracedLog(LOGGER, Level.SEVERE, "Couldn't fetch all the activities", err);
+            DB_CONNECTION.closeConnection();
+        }
         return activities;
     }
 
     public static void createNewActivity(final ActivityData activityData) {
-        // TODO: Complete method and query
+        try {
+            DB_CONNECTION.openConnection();
+            DB_CONNECTION.setQueryData(CREATE_ACTIVITY, activityData.name(), activityData.description());
+        } catch (final SQLException err) {
+            bracedLog(LOGGER, Level.SEVERE, "Couldn't create a new activity " + activityData.name(), err);
+            DB_CONNECTION.closeConnection();
+        }
     }
 
     public static void deleteActivity(final ActivityData activityData) {
-        // TODO: Complete method and query
+        try {
+            DB_CONNECTION.openConnection();
+            DB_CONNECTION.setQueryData(DELETE_ACTIVITY, activityData.name());
+        } catch (final SQLException err) {
+            bracedLog(LOGGER, Level.SEVERE, "Couldn't delete the activity " + activityData.name(), err);
+            DB_CONNECTION.closeConnection();
+        }
     }
 
     public static Set<ReviewData> getReviewsFromActivity() {
