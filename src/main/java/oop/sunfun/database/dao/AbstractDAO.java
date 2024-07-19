7b59cf6854f5d059f3eaff03cb28fe38eb4ae5e1
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-07-19 20:43:23 +0200

    Add javadoc to AbstractDAO

--- src/main/java/oop/sunfun/database/dao/AbstractDAO.java
+++ src/main/java/oop/sunfun/database/dao/AbstractDAO.java
@@ -8,11 +8,22 @@ import java.util.logging.Logger;
 
 public abstract class AbstractDAO {
 
+    /**
+     * The connection to the database, protected to assure it's only one (for performance).
+     */
     protected static final IDatabaseConnection DB_CONNECTION;
+
     static {
         DB_CONNECTION = SunFunDatabase.getDatabaseInstance();
     }
 
+    /**
+     * Method used to properly log errors within the various DAO objects.
+     * @param logger The logger used to log.
+     * @param logLevel The level at which to log the error at.
+     * @param message The message to log.
+     * @param error The error to trace with the log.
+     */
     public static void bracedLog(final Logger logger, final Level logLevel, final String message,
                                  final Throwable error) {
         if (logger.isLoggable(logLevel)) {
@@ -20,6 +31,12 @@ public abstract class AbstractDAO {
         }
     }
 
+    /**
+     * Method used to properly log errors within the various DAO objects.
+     * @param logger The logger used to log.
+     * @param logLevel The level at which to log the error at.
+     * @param message The message to log.
+     */
     public static void bracedLog(final Logger logger, final Level logLevel, final String message) {
         if (logger.isLoggable(logLevel)) {
             logger.log(logLevel, message);
